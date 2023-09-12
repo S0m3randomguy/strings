bca7107b2708f69072968f02fca59a471482f873
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-09-12 20:37:07 +0200

    Added whitespace constant. Experimental use of the \t escape char, viable to change later

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ namespace strings {
     export const lowercase = "abcdefghijklmnopqrstuvwxyz";
     export const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     export const special = "!\"#$%&'()*+,-./:;<=>?@[\]^_{|}~`";
+    export const whitespace = " \t";
     export const letters = lowercase + uppercase;
     export const alphanumeric = letters + digits;
     export const ascii = alphanumeric + special;
