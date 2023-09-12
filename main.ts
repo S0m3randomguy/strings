5f70160e57754861f3b918f2c044e69350f45040
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-09-12 20:20:53 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+
+namespace strings {
+    export const digits = "0123456789";
+    export const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    export const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    export const special = "!\"#$%&'()*+,-./:;<=>?@[\]^_{|}~`";
+    export const letters = lowercase + uppercase;
+    export const alphanumeric = letters + digits;
+    export const ascii = alphanumeric + special;
+}
